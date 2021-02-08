934719f2932518f210ea5243b1e8b27b66edf0c1
developer-zolon <zolonchandro@gmail.com>
2021-02-08 13:23:43 +0600

    taskList
    javaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+form = document.querySelector('form');
+input = document.querySelector('#input-add');
+filter = document.querySelector('#input');
+list = document.querySelector('#task-list');
+clearBtn = document.querySelector('clear-btn');
+
+form.
